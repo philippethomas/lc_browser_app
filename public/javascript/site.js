b08ff17eb0b48c6c63f41cfc8a4d4b03e9ed27b7
R. Bryan Hughes <rbhughes@logicalcat.com>
2013-07-29 17:34:53 -0700

    reload page after crawl

--- public/javascript/site.js
+++ public/javascript/site.js
@@ -25,8 +25,7 @@ jQuery(function($){
     $.post('/setWorkStatus', {working: 'no'},  function(data){
       if (data.working != 'no') { console.error('problem setting work status'); }
       workStatus();
-      console.log('would have reloaded......');
-      //location.reload(true);
+      location.reload(true);
     });
   });
 
